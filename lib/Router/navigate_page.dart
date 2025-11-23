4f1fe4694563477141ac6916a174b9cb9b298d99
javahuhu <cgelera3@gmail.com>
2025-11-23 14:30:30 +0800

    add a bloc logic for adding the input city name in this page

--- lib/Router/navigate_page.dart
+++ lib/Router/navigate_page.dart
@@ -1,6 +1,12 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:weather_app/Data/DataSource/weather_datasource.dart';
+import 'package:weather_app/Data/Repositories/repositories_implementation/weather_repositories_implementation.dart';
+import 'package:weather_app/Presentation/Bloc/weather_bloc.dart';
+import 'package:weather_app/Presentation/Bloc/weather_event.dart';
 import 'package:weather_app/Presentation/View/splashscreen.dart';
 import 'package:weather_app/Presentation/View/getstarted.dart';
+import 'package:weather_app/Presentation/View/search_weather.dart';
 import 'package:weather_app/Presentation/View/weather.dart';
 // <-- your target page
 
@@ -21,10 +27,32 @@ final GoRouter appRouter = GoRouter(
     ),
 
     GoRoute(
+      path: '/saerchweather',
+      name: 'searchweather',
+      builder: (context, state) {
+        return const SearchWeatherPage();
+      },
+    ),
+
+    GoRoute(
       path: '/weather',
       name: 'weather',
       builder: (context, state) {
-        return const WeatherPage();
+        final cityName = state.extra as String;
+
+        return BlocProvider(
+          create: (_) {
+            final datasource = WeatherDatasource();
+            final repo = WeatherRepositoriesImplementation(
+              weatherDatasource: datasource,
+            );
+
+           
+            return WeatherBloc(repository: repo)
+              ..add(FetchWeatherLocation(cityName: cityName));
+          },
+          child: WeatherPageScreen(cityName: cityName),
+        );
       },
     ),
   ],
