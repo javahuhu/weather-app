bc27ad46321af5755ef5ddb7267ff58f5cfddf3a
javahuhu <cgelera3@gmail.com>
2025-11-23 14:22:00 +0800

    Adjust the model according to needed details

--- lib/Data/Models/weather_model.dart
+++ lib/Data/Models/weather_model.dart
@@ -8,16 +8,29 @@ class WeatherModel extends WeatherEntity {
     required super.description,
     required super.humidity,
     required super.windSpeed,
+    required super.localdateTime,
+    required super.timzeZOne,
   });
 
   factory WeatherModel.fromJson(Map<String, dynamic> json) {
+    final int dt = json['dt'] as int;
+    final int timeZone = json['timezone'] as int;
+
+    final DateTime utcTime = DateTime.fromMillisecondsSinceEpoch(
+      dt * 1000,
+      isUtc: true,
+    );
+    final DateTime localTime = utcTime.add(Duration(seconds: timeZone));
+
     return WeatherModel(
-      cityName: json['name'],
-      condition: json['weather'][0]['main'],
+      cityName: json['name'] as String,
+      condition: (json['weather'] as List).first['main'] as String,
       temperature: (json['main']['temp'] as num).toDouble(),
-      description: json['weather'][0]['description'],
-      humidity: (json['main']['humidity'] as num).toDouble(),
+      description: json['weather'][0]['description'] as String,
+      humidity: (json['main']['humidity'] as num).toInt(),
       windSpeed: (json['wind']['speed'] as num).toDouble(),
+      localdateTime: localTime,
+      timzeZOne: timeZone,
     );
   }
 }
