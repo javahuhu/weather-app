66a191b55f2bd72d2dd86fc9f42cee7704aa7013
javahuhu <cgelera3@gmail.com>
2025-11-23 14:23:59 +0800

    Calling the DataSource for implementation

---
+++ lib/Data/Repositories/repositories_implementation/weather_repositories_implementation.dart
@@ -0,0 +1,17 @@
+import 'package:weather_app/Data/DataSource/weather_datasource.dart';
+import 'package:weather_app/Data/Models/weather_model.dart';
+import 'package:weather_app/Data/Repositories/repositories_abstract/weather_repositories_abstract.dart';
+import 'package:weather_app/Domain/Entities/weather_entity.dart';
+
+class WeatherRepositoriesImplementation extends WeatherRepositoriesAbstract {
+  final WeatherDatasource weatherDatasource;
+
+  WeatherRepositoriesImplementation({required this.weatherDatasource});
+
+  @override
+  Future<WeatherEntity> getWeatherLocation({required String cityName}) async {
+    final WeatherModel wModel = await weatherDatasource.getWeather(cityName);
+
+    return wModel;
+  }
+}
